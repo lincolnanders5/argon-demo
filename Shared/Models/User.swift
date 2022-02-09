48ff1c51be8b0c563a6cdfa0d6b15d9b3a8eec06
Lincoln Anders <lincolnanders5@gmail.com>
2022-02-09 01:33:52 -0500

    added base urls to objects

--- Shared/Models/User.swift
+++ Shared/Models/User.swift
@@ -10,12 +10,14 @@ import Argon
 import SerializedSwift
 
 final class User: ARModel {
+    static var baseURL = "http://192.168.1.151:3000/users"
 	@Serialized var email: String?
 	
 	public required init() {}
 }
 
 final class Admin: ARModel {
+    static var baseURL = "http://192.168.1.151:3000/admins"
     @Serialized var email: String?
     
     public required init() {}
